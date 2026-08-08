64f36239cf3f5bda2e13b3fa854efb4d649a2880
yakubets_v <yakubets_v@icloud.com>
2026-08-08 22:03:53 +0200

    Initial Commit

---
+++ Trans/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trans
+//
+//  Created by Uladzislau Yakubets on 08/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trans/TransApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransApp.swift
+//  Trans
+//
+//  Created by Uladzislau Yakubets on 08/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct TransApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
